1f48ebc4b0d0fb3e4bf14c9a0a24ad2619cf159a
Anuvab Pal <your_email@example.com>
2026-06-20 16:24:39 +0530

    Initial Spring Boot project setup with Postgres config

---
+++ src/main/java/com/smarterp/SmarterpBackendApplication.java
@@ -0,0 +1,13 @@
+package com.smarterp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmarterpBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmarterpBackendApplication.class, args);
+	}
+
+}
